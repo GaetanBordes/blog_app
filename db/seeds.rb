1caea751b26a81d4ed1bc697555080c77bc83269
GaetanBordes <bordes.gaetan33@gmail.com>
2025-06-30 10:45:20 +0200

    devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 # db/seeds.rb
-
 puts "Nettoyage de la base de données..."
 Article.destroy_all
+User.destroy_all
 
-puts "Création de 30 articles..."
+puts "Création d'un utilisateur de test..."
+user = User.create!(
+  email: "test@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+puts "Création de 30 articles pour l'utilisateur #{user.email}..."
 30.times do
-  Article.create!(
+  user.articles.create!(
     title: Faker::Book.title,
     content: Faker::Lorem.paragraph(sentence_count: 15)
   )
